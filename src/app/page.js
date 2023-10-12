f5190d12d30a4163319036d1d81d7424580f4829
DavySousa <84909226+DavySousa017@users.noreply.github.com>
2023-10-11 23:52:43 -0300

    [UPDATE] ProducSection
    e.e

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import ProductSection from "./components/ProductSection";
+import Provider from "./context/Provider";
 
 export default function Home() {
   return (
     <main className="bg-bg min-h-screen">
+      <Provider>
       <Header/>
+      <ProductSection/>
+      </Provider>
     </main>
   );
 }
