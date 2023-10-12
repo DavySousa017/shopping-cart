8cd2be6f52aeb5cbfb7fe952cc5f982e88fec0d1
DavySousa <84909226+DavySousa017@users.noreply.github.com>
2023-10-12 15:22:09 -0300

    [UPDATE] IconCart

--- src/app/components/IconCart.jsx
+++ src/app/components/IconCart.jsx
@@ -5,10 +5,11 @@ import appContext from '../context/appContext'
 
 const IconCart = () => {
 
-  const { cartIsVisible, setCartIsVisible } = useContext(appContext)
+  const { cartIsVisible, setCartIsVisible, productsInCart } = useContext(appContext)
 
   return (
     <button onClick={() => setCartIsVisible(!cartIsVisible)} type='button' className={`flex items-center relative`}>
+      {productsInCart.length == 0 && '' || productsInCart.length > 0 && <span className='absolute -top-2 -right-2 flex items-center justify-center w-[20px] h-[20px] bg-red-600 text-xs rounded-full font-bold animate-pulse'>{productsInCart.length}</span>}
       <PiShoppingCart className='text-3xl text-white' />
     </button>
   )
